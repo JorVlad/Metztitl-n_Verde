b735be2dba68f7215c76ed66661ea76205a35784
JorVlad <jordan.vlad.bh@gmail.com>
2025-11-03 20:46:01 -0600

    Add files via upload
    Última versión!

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open("index.html", "r", encoding="utf-8") as f:
+        html = f.read()
+    return html
 
-if __name__ == '__main__':
+@app.route('/<path:filename>')
+def serve_static(filename):
+    # servir archivos CSS, JPG, PNG o JS del mismo nivel
+    if filename.endswith(".css"):
+        with open(filename, "r", encoding="utf-8") as f:
+            return Response(f.read(), mimetype="text/css")
+    elif filename.lower().endswith((".jpg", ".jpeg", ".png", ".gif")):
+        with open(filename, "rb") as f:
+            return Response(f.read(), mimetype="image/jpeg")
+    elif filename.endswith(".js"):
+        with open(filename, "r", encoding="utf-8") as f:
+            return Response(f.read(), mimetype="application/javascript")
+    else:
+        return "Archivo no permitido", 404
+
+if __name__ == "__main__":
     app.run(debug=True)
